09e5f6e788a90ca8f1c3d2e53c173b7dd5d6aed7
aman.shrivastava <aman.shrivastava@flipkart.com>
2020-03-05 00:12:38 +0530

    adding models

---
+++ parking_lot/src/main/java/com/parking/models/Vehicle.java
@@ -0,0 +1,39 @@
+package com.parking.models;
+
+public class Vehicle {
+
+    private String registrationNumber;
+
+    private String color; //can be a enum
+
+    private ParkingSpot parkingSpot;
+
+    public Vehicle(String regitrationNumber, String color) {
+        this.registrationNumber = regitrationNumber;
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+}
