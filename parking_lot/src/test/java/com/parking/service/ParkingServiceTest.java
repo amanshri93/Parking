dda7fc134eec0b20c3a8d8a2bdc160753108296c
aman.shrivastava <aman.shrivastava@flipkart.com>
2020-03-05 01:13:03 +0530

    mvn added and test case fix

--- parking_lot/src/test/java/com/parking/service/ParkingServiceTest.java
+++ parking_lot/src/test/java/com/parking/service/ParkingServiceTest.java
@@ -2,59 +2,61 @@ package com.parking.service;
 
 import com.parking.helper.Constants;
 import com.parking.models.Vehicle;
+import com.parking.service.ParkingService;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ParkingLotControllerTest {
+public class ParkingServiceTest {
 
-    ParkingLotController parkingLotController;
+    ParkingService parkingService;
     Vehicle vehicle = new Vehicle("123", "red");
     Vehicle vehicle1 = new Vehicle("1234", "blue");
     Vehicle vehicle2 = new Vehicle("1235", "red");
     @Before
     public void setUp() throws Exception {
-        parkingLotController = new ParkingLotController(2);
+        parkingService = new ParkingService(2);
     }
 
     @Test
     public void park() throws Exception {
-        Assert.assertEquals(String.format(Constants.ALLOCATED_SLOT, 1), parkingLotController.park(vehicle));
-        Assert.assertEquals(String.format(Constants.ALLOCATED_SLOT, 2), parkingLotController.park(vehicle1));
-        Assert.assertEquals(Constants.NO_SPACE, parkingLotController.park(vehicle2));
+        Assert.assertEquals(String.format(Constants.ALLOCATED_SLOT, 1), parkingService.park(vehicle));
+        Assert.assertEquals(String.format(Constants.ALLOCATED_SLOT, 2), parkingService.park(vehicle1));
+        Assert.assertEquals(Constants.NO_SPACE, parkingService.park(vehicle2));
     }
 
     @Test
     public void removeVehicle() throws Exception {
-        parkingLotController.park(vehicle);
-        parkingLotController.park(vehicle1);
-        Assert.assertEquals(String.format(Constants.SLOT_FREED, 2), parkingLotController.removeVehicle(2));
-        Assert.assertEquals(String.format(Constants.SLOT_FREED, 1), parkingLotController.removeVehicle(1));
-        Assert.assertEquals(Constants.ALREADY_EMPTY, parkingLotController.removeVehicle(2));
+        parkingService.park(vehicle);
+        parkingService.park(vehicle1);
+        Assert.assertEquals(String.format(Constants.SLOT_FREED, 2), parkingService.removeVehicle(2));
+        Assert.assertEquals(String.format(Constants.SLOT_FREED, 1), parkingService.removeVehicle(1));
+        Assert.assertEquals(Constants.ALREADY_EMPTY, parkingService.removeVehicle(2));
     }
 
     @Test
     public void slotNumberForRegistrationNumber() throws Exception {
-        parkingLotController.park(vehicle);
-        parkingLotController.park(vehicle1);
-        Assert.assertEquals("1", parkingLotController.slotNumberForRegistrationNumber("123"));
-        Assert.assertEquals(Constants.NOT_PARKED_IN_PARKING_LOT, parkingLotController.slotNumberForRegistrationNumber("1"));
+        parkingService.park(vehicle);
+        parkingService.park(vehicle1);
+        Assert.assertEquals("1", parkingService.slotNumberForRegistrationNumber("123"));
+        Assert.assertEquals(Constants.NOT_PARKED_IN_PARKING_LOT, parkingService.slotNumberForRegistrationNumber("1"));
     }
 
     @Test
     public void slotNumbersForCarsWithColour() throws Exception {
-        parkingLotController.park(vehicle);
-        parkingLotController.park(vehicle1);
-        Assert.assertEquals("1", parkingLotController.slotNumbersForCarsWithColour("red"));
-        Assert.assertEquals(Constants.COLOR_CAR_NOT_PARKED_IN_PARKING_LOT, parkingLotController.slotNumbersForCarsWithColour("white"));
+        parkingService.park(vehicle);
+        parkingService.park(vehicle1);
+        Assert.assertEquals("1", parkingService.slotNumbersForCarsWithColour("red"));
+        Assert.assertEquals(Constants.COLOR_CAR_NOT_PARKED_IN_PARKING_LOT, parkingService.slotNumbersForCarsWithColour("white"));
     }
 
     @Test
     public void registrationNumbersForCarsWithColour() throws Exception {
-        parkingLotController.park(vehicle);
-        parkingLotController.park(vehicle1);
-        Assert.assertEquals("123", parkingLotController.registrationNumbersForCarsWithColour("red"));
-        Assert.assertEquals(Constants.COLOR_CAR_NOT_PARKED_IN_PARKING_LOT, parkingLotController.registrationNumbersForCarsWithColour("white"));
+        parkingService.park(vehicle);
+        parkingService.park(vehicle1);
+        Assert.assertEquals("123", parkingService.registrationNumbersForCarsWithColour("red"));
+        Assert.assertEquals(Constants.COLOR_CAR_NOT_PARKED_IN_PARKING_LOT, parkingService.registrationNumbersForCarsWithColour("white"));
     }
 
 }
